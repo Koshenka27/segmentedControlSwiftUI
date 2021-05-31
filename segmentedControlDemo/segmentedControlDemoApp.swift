c7ae6ae32188536f2f27c6eedcf9e2fc6cfa1869
Logan Koshenka <koshenka27@gmail.com>
2021-05-30 22:44:09 -0400

    Initial Commit

---
+++ segmentedControlDemo/segmentedControlDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  segmentedControlDemoApp.swift
+//  segmentedControlDemo
+//
+//  Created by Logan Koshenka on 5/30/21.
+//
+
+import SwiftUI
+
+@main
+struct segmentedControlDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
